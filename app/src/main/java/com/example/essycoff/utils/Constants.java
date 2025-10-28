e4ff27a23a2891318a9febe772bd201b7c1e3857
RifqiArdian09~ <kambingjawa009@gmail.com>
2025-10-28 19:04:37 +0700

    update

--- app/src/main/java/com/example/essycoff/utils/Constants.java
+++ app/src/main/java/com/example/essycoff/utils/Constants.java
@@ -1,8 +1,8 @@
 package com.example.essycoff.utils;
 
 public class Constants {
-    public static final String SUPABASE_URL = "//";
-    public static final String SUPABASE_ANON_KEY = "//";
+    public static final String SUPABASE_URL = "https://wflqryqxkuljkgbglqrq.supabase.co";
+    public static final String SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndmbHFyeXF4a3VsamtnYmdscXJxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTU0MzIxNzksImV4cCI6MjA3MTAwODE3OX0.9fZc8DaKcRPPL87uq2V4_2LSHbkZaltHErTaw9OElU4";
     public static final String PREF_NAME = "essycoff_prefs";
     public static final String KEY_TOKEN = "auth_token";
     public static final String KEY_EMAIL = "user_email";
